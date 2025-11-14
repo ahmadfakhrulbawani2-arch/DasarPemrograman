ec89384e366940fd5d009ecf8dc1351a2db5b714
Ahmad Fakhrul Bawani <ahmadfakhrulbawani2@gmail.com>
2025-11-14 21:16:30 +0700

    Archived Nov 14, 2025

---
+++ seslab22.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a = 0;
+    scanf("%d", &a);
+
+    for(int i = 1; i <= a; i++) {
+        if(i % 2 == 0) {
+            printf("* ");
+        } else {
+            printf("%d ", i);
+        }
+    }
+    return 0;
+}
